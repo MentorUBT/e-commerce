70b19f2aaadc290572c8e3749bd8bcecea78c9c2
Mentor Gashi <mg30394@ubt-uni.net>
2022-01-03 10:28:14 +0100

    Additional files added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,42 @@
-const accordion = document.getElementsByClassName('faq-wrapper');
+const form = document.getElementById('form');
+const firstName = document.getElementById('fname');
+const lastName = document.getElementById('lname');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const errorElement = document.getElementById('error');
 
-for (i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}
+form.addEventListener('submit', (e) => {
+  let messages = []
+  if (fname.value === '' || fname.value == null) {
+    messages.push('First name is required!')
+  }
+
+  if (lname.value === '' || lname.value == null) {
+    messages.push('Last name is required!')
+  }
+
+  if (email.value === '' || email.value == null) {
+    messages.push('Email address is required!')
+  }
+
+  if (!email.contains === '!') {
+    messages.push('Email must contain an "@" symbol!')
+  }
+
+  if (password.value.length <= 6) {
+    messages.push('Password must be longer than 6 characters!')
+  }
+
+  if (password.value.length >= 20) {
+    messages.push('Password must be less than 20 characters!')
+  }
+
+  if (password.value === 'password') {
+    messages.push('Password cannot be "password"!');
+  }
+
+  if (messages.length > 0) {
+    e.preventDefault();
+    errorElement.innerText = messages.join('\n'); 
+  }
+});
---
+++ assets/js/slider.js
@@ -0,0 +1,10 @@
+$(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: false,
+    items: 1,
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 7000,
+    autoplayHoverPause: true,
+    dots: true,
+  });
