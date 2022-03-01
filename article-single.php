a4764308d3101079cebbc6c758a00b56be317bad
Vigan Zhuri <vz52016@ubt-uni.net>
2022-03-01 12:27:43 +0100

    News.php and Article-single added

---
+++ article-single.php
@@ -0,0 +1,170 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600&family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+<header>
+        <nav class="homepage">
+            <div class="logo">
+                <h1>Company</h1>
+            </div>
+            <ul class="nav-menu">
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="#!">Products <span>+</span></a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="products.php">All products</a>
+                        </li>
+                        <li>
+                            <a href="single-product.php">Single product</a>
+                        </li>
+                        <li>
+                            <a href="coming-soon.php">Coming Soon</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="news.php">News</a>
+                </li>
+                <li>
+                    <a href="contact.php">Contact</a>
+                </li>
+            </ul>
+            <div class="authentication">
+                <ul>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="logout.php">Logout</a>
+                    </li>
+                    <li>
+                        <a href="register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="article-single e-padding">
+            <div class="container">
+                <div class="row">
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/1-1.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <h2>Apple Watch Series Review</h2>
+                            <ul>
+                                <li id="article-author">
+                                    John Doe
+                                </li>
+                                <li id="article-date">
+                                    28.02.2022
+                                </li>
+                            </ul>
+                            <p>The Apple Watch Series 7 is Apple's latest smartwatch, featuring a larger always-on Retina display, a more rounded design with a larger casing, improved durability, and faster charging, starting at a price of $399. Announced in September 2021, the Apple Watch Series 7 is the newest Apple Watch in Apple's lineup and it is fairly early in its product cycle. Apple tends to release new Apple Watch models every September, and there is no reason to suggest that a new Apple Watch Series 8 will not launch this year. There are early signs of Apple Watch Series 8 models with a number of upgrades and improvements, but since this is likely just under a year away, now is still a good time to buy the Apple Watch Series 7. While the Apple Watch Series 7 is Apple's most full-featured, high-end smartwatch for those who want features like blood oxygen monitoring, ECG, an always-on display, and more premium finishes, users who are looking for a more affordable option should consider the Apple Watch SE. Starting at $279, the Apple Watch SE offers many key Apple Watch features, such as an optical heart rate sensor and fall detection, but at a lower price that balances functionality and affordability. On the other hand, if price is your main concern and you do not need advanced health functions, the Apple Watch Series 3 may be more appropriate than the $399 Apple Watch Series 7 as it offers many of the Apple Watch's core features for just $199. There are some tradeoffs with the Apple Watch Series 3 because it is a much older model, such as a smaller display, an older chipset, and the lack of a compass, fall detection, ECG, and blood oxygen monitoring. The Apple Watch Series 7, announced in September 2021, is the current iteration of the Apple Watch that originally launched in 2015 and replaced the Series 6. The Apple Watch Series 7 builds on the design of previous Apple Watch models with a more rounded design and offers some notable new features including larger displays, improved durability, and faster charging. The Apple Watch Series 7 is available in new 41 and 45mm size options that are 1mm larger than the 40mm and 44mm options of previous generations, and the casings have been refined with softer, more rounded edges. Like the Apple Watch Series 6, Series 7 models feature a black ceramic and sapphire crystal backing and a Digital Crown with haptic feedback. The Digital Crown has a built-in sensor for taking ECG readings. The new models feature a larger, re-engineered Retina display with more screen area due to slimmer borders. The display has a unique refractive edge that almost curves to the casing. There are interface improvements and two unique watch faces to take advantage of the larger displays. The Series 7 continues to feature the low power (LTPO) OLED Always-On display technology introduced with the Series 5, allowing users to see their watch face and complications at all times. The Apple Watch Series 7 is more durable than previous models, with crack-resistant front glass, IP6X dust resistance, and a WR50 water resistance rating. Apple Watch Series 7 models are also able to charge 33 percent faster, with just eight minutes of charging providing up to eight hours of sleep tracking time.</p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="e-padding">
+        <div class="container">
+            <div class="row">
+                <article>
+                    <h2>Company</h2>
+                    <ul>
+                        <li>
+                            <p>About Us</p>
+                        </li>
+                        <li>
+                            <p>Our Team</p>
+                        </li>
+                        <li>
+                            <p>Blogs</p>
+                        </li>
+                        <li>
+                            <p>Gift Cards</p>
+                        </li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Services</h2>
+                    <ul>
+                        <li>
+                            <p>Shipping</p>
+                        </li>
+                        <li>
+                            <p>Tracking</p>
+                        </li>
+                        <li>
+                            <p>Returns</p>
+                        </li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Contact</h2>
+                    <ul>
+                        <li>
+                            <p>Email: info@company.com</p>
+                        </li>
+                        <li>
+                            <p>Phone: +181 65 87 66 33 15</p>
+                        </li>
+                        <li>
+                            <p>Address: New York, Queens, 21st</p>
+                        </li>
+                    </ul>
+                </article>
+                <article class="social-media">
+                    <h2>Follow Us</h2>
+                    <ul>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-facebook"></ion-icon>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-twitter"></ion-icon>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-instagram"></ion-icon>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-linkedin"></ion-icon>
+                            </a>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/slider.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+</html>
---
+++ news.php
@@ -0,0 +1,231 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Raleway:wght@400&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600&family=Raleway:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <header>
+        <nav class="homepage">
+            <div class="logo">
+                <h1>Company</h1>
+            </div>
+            <ul class="nav-menu">
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    <a href="#!">Products <span>+</span></a>
+                    <ul class="sub-menu">
+                        <li>
+                            <a href="products.php">All products</a>
+                        </li>
+                        <li>
+                            <a href="single-product.php">Single product</a>
+                        </li>
+                        <li>
+                            <a href="coming-soon.php">Coming Soon</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="news.php">News</a>
+                </li>
+                <li>
+                    <a href="contact.php">Contact</a>
+                </li>
+            </ul>
+            <div class="authentication">
+                <ul>
+                    <li>
+                        <a href="login.php">Login</a>
+                    </li>
+                    <li>
+                        <a href="logout.php">Logout</a>
+                    </li>
+                    <li>
+                        <a href="register.php">Register</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section class="news e-padding">
+            <div class="container">
+                <div class="row">
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/1.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">Apple Watch Series Review</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/2.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">Cisco faces a $14B backlog thanks to component scarcity</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/3.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">iPad Air: Apple's best all-around tablet is about to get better</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/4.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">Tougher IPO norms for loss-making tech companies</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/5.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">COVID-19: B.C. employers no longer need to allow workers to work from home</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/6.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">Apple iPhone 14 series expected to get 8GB RAM</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/7.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">10 Reasons Why Python is One of the Best Programming Languages</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <div class="article-image">
+                            <img src="assets/images/news/8.jpg" alt="">
+                        </div>
+                        <div class="article-content">
+                            <a href="article-single.php">Supply chain woes forcing more workloads to the cloud</a>
+                            <p>The quick, brown fox jumps over a lazy dog. DJs flock by when MTV ax quiz prog. Junk MTV quiz graced by fox whelps. Bawds jog, flick quartz, vex nymphs. Waltz, bad.</p>
+                        </div>
+                    </article>
+                </div>
+                <div class="view-more-btn">
+                    <a href="#">View More</a>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer class="e-padding">
+        <div class="container">
+            <div class="row">
+                <article>
+                    <h2>Company</h2>
+                    <ul>
+                        <li>
+                            <p>About Us</p>
+                        </li>
+                        <li>
+                            <p>Our Team</p>
+                        </li>
+                        <li>
+                            <p>Blogs</p>
+                        </li>
+                        <li>
+                            <p>Gift Cards</p>
+                        </li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Services</h2>
+                    <ul>
+                        <li>
+                            <p>Shipping</p>
+                        </li>
+                        <li>
+                            <p>Tracking</p>
+                        </li>
+                        <li>
+                            <p>Returns</p>
+                        </li>
+                    </ul>
+                </article>
+                <article>
+                    <h2>Contact</h2>
+                    <ul>
+                        <li>
+                            <p>Email: info@company.com</p>
+                        </li>
+                        <li>
+                            <p>Phone: +181 65 87 66 33 15</p>
+                        </li>
+                        <li>
+                            <p>Address: New York, Queens, 21st</p>
+                        </li>
+                    </ul>
+                </article>
+                <article class="social-media">
+                    <h2>Follow Us</h2>
+                    <ul>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-facebook"></ion-icon>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-twitter"></ion-icon>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-instagram"></ion-icon>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#!">
+                                <ion-icon name="logo-linkedin"></ion-icon>
+                            </a>
+                        </li>
+                    </ul>
+                </article>
+            </div>
+        </div>
+    </footer>
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/script.js"></script>
+    <script src="assets/js/slider.js"></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+</body>
+
+</html>
